895098d932bca8482dad5057b14e0459398f7625
alperenasln <70862043+alperenasln@users.noreply.github.com>
2021-04-24 03:38:08 +0300

    Update Edge.java

--- src/Edge.java
+++ src/Edge.java
@@ -1,9 +1,6 @@
 //-----------------------------------------------------
 //Title: Edge
 //Author:Alperen Aslan
-//ID: 27226264696
-//Section: 2
-//Assignment: 4
 //Definition : This class is implemented from book.
 public class Edge implements Comparable<Edge> { 
 
